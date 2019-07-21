ce1eb216695fc93d9dcaf64451bcc351c38ca347
Jonas R <joroec@gmx.net>
2019-07-21 21:39:59 +0200

    Update types.go
    Added comments.

--- pkg/virt/types.go
+++ pkg/virt/types.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2019 The virnsnap authors. See file "AUTHORS".
+// Licensed under the MIT License. You have obtained a copy of the License at
+// the "LICENSE" file in this repository.
+
+// Package virt implements high-level functions for handling virtual machines
+// (VMS) that use the more low-level libvirt functions internally.
 package virt
 
 // Logger interface provides an abstraction over different loggers
